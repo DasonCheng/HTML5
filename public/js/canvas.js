c7118c75b38fd79d7bfecc9f25539a4f537c4b42
DasonCheng <dasoncheng@foxmail.com>
2016-06-13 17:22:08 +0800

    canvas

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,14 +1,18 @@
 /**
  * Created by Origin on 6/13/16.
  */
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
 
-context.fillRect(0, 0, 100, 100);
-context.strokeRect(120, 0, 100, 100);
-context.fillStyle = "red";
-context.strokeStyle = "blue";
-
-context.moveTo(0,0);
-context.lineTo(200,100);
-context.stroke();
+// ctx.fillRect(0, 0, 100, 100);
+// ctx.strokeRect(120, 0, 100, 100);
+// ctx.fillStyle = "red";
+// ctx.strokeStyle = "blue";
+//
+// ctx.moveTo(0,0);
+// ctx.lineTo(200,100);
+// ctx.stroke();
+ctx.font="30px Arial";
+//实心
+ctx.fillText("Hello World",10,50);
+ctx.strokeText("Hello World",10,100);
